79ece12e706a681a63135a7c43304ba9936013c5
Your Name <you@example.com>
2023-07-21 06:39:16 -0700

    aa

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
+
 /**
- * main - Entry point 
- *Description: 'prints _putchat to stdout'
- * Return always 0
+ * main - Entry point
+ *
+ * Description: Prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
-      _putchar("_");
-      _putchar("p");
-      _putchar("u");
-      _putchar("t");
-      _putchar("c");
-      _putchar("h");
-      _putchar("a");
-      _putchar("r");
-      _putchar('\n');
-       returm(0);
+char str[] = "_putchar";
+int ch;
+
+for (ch = 0; ch < 8; ch++)
+	_putchar(str[ch]);
+_putchar('\n');
+return (0);
 }
